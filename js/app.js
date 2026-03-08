ebf70f04c20a5cfa3328114e192d4f8176289af1
Md Injamamul Hoq Tamim <injamamulhoqtamim@gmail.com>
2026-03-09 01:11:13 +0600

    message

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const user = document.getElementById("username").value;
 const pass = document.getElementById("password").value;
 
 if(user === "admin" && pass === "admin123"){
-window.location.href = "dashboard.html";
+window.location.href = "./dashboard.html";
 }
 else{
 alert("Invalid Credentials");
